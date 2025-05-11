12d4c7fb0c7f1ecd73446e0d52427f8236782f7e
Pablo S <cineschonffeldt@gmail.com>
2025-05-11 17:37:06 -0400

    Refactor to update platform limits and dom

--- script.js
+++ script.js
@@ -8,14 +8,21 @@ const sentencesNumberEl = document.querySelector('.stat__number--sentences');
 const paragraphsNumberEl = document.querySelector('.stat__number--paragraphs');
   
 // DOM social media
-const twitterNumberEl = document.querySelector('.stat__number--twitter');
-const facebookNumberEl = document.querySelector('.stat__number--facebook');
-const tiktokNumberEl = document.querySelector('.stat__number--tiktok');
-const instagramNumberEl = document.querySelector('.stat__number--instagram');
-const linkedinNumberEl = document.querySelector('.stat__number--linkedin');
-const youtubeNumberEl = document.querySelector('.stat__number--youtube');
-const pinterestNumberEl = document.querySelector('.stat__number--pinterest');
-const redditNumberEl = document.querySelector('.stat__number--reddit');
+// const twitterNumberEl = document.querySelector('.stat__number--twitter');
+// const facebookNumberEl = document.querySelector('.stat__number--facebook');
+// const tiktokNumberEl = document.querySelector('.stat__number--tiktok');
+// const instagramNumberEl = document.querySelector('.stat__number--instagram');
+// const linkedinNumberEl = document.querySelector('.stat__number--linkedin');
+// const youtubeNumberEl = document.querySelector('.stat__number--youtube');
+// const pinterestNumberEl = document.querySelector('.stat__number--pinterest');
+// const redditNumberEl = document.querySelector('.stat__number--reddit');
+
+const platforms = ['twitter', 'facebook', 'tiktok', 'instagram', 'linkedin', 'youtube', 'pinterest', 'reddit'];
+
+const platformEls = Object.fromEntries(
+  platforms.map(p => [p, document.querySelector(`.stat__number--${p}`)])
+);
+
 
 
 const inputHandler = () => {
@@ -33,19 +40,20 @@ const inputHandler = () => {
   if (textareaEl.value.length === 0) {
     numberOfWords = 0;
   }
-  const numberOfCharacters = textareaEl.value.length;
-  // const numberOfCharacters = textareaEl.value.replace(/\s/g, '').length;
-
-
-
+  // determine number of characters (not counting \n) 
+  const numberOfCharacters = textareaEl.value.replace(/\n/g, '').length;
+  
+  
+  // determine number of sentences (include ending with [.!?])[.!?](?![.!?])/g) || [])
   const text = textareaEl.value;
   const numberOfSentences = (text.match(/(?<![.!?])[.!?](?![.!?])/g) || []).length;
 
-
+  // determine number of paragraphs
   const newlineMatches = text.match(/\n/g);
   const numberOfParagraphs = newlineMatches ? newlineMatches.length : 0;
-
-
+  
+  
+  // determine number of social media limits
   const twitterCharactersLeft = 280 - numberOfCharacters;
   const facebookCharactersLeft = 63206 - numberOfCharacters;
   const tiktokCharactersLeft = 4000 - numberOfCharacters;
@@ -62,64 +70,28 @@ const inputHandler = () => {
     charactersNumberEl.classList.remove('alt');
   }
 
-  // add visual indicator if limit is exceeded
-  if (twitterCharactersLeft < 0) {
-    twitterNumberEl.classList.add('stat__number--limit');
-  } else {
-    twitterNumberEl.classList.remove('stat__number--limit');
-  }
+  // Utility to update platform limits and class toggles
+  const updateSocialStat = (element, limitLeft) => {
+  element.textContent = limitLeft;
+  element.classList.toggle('stat__number--limit', limitLeft < 0);
+  };
 
-  if (facebookCharactersLeft < 0) {
-    facebookNumberEl.classList.add('stat__number--limit');
-  } else {
-    facebookNumberEl.classList.remove('stat__number--limit');
-  }
+  updateSocialStat(platformEls.twitter, twitterCharactersLeft);
+  updateSocialStat(platformEls.facebook, facebookCharactersLeft);
+  updateSocialStat(platformEls.tiktok, tiktokCharactersLeft);
+  updateSocialStat(platformEls.instagram, instagramCharactersLeft);
+  updateSocialStat(platformEls.linkedin, linkedinCharactersLeft);
+  updateSocialStat(platformEls.youtube, youtubeCharactersLeft);
+  updateSocialStat(platformEls.pinterest, pinterestCharactersLeft);
+  updateSocialStat(platformEls.reddit, redditCharactersLeft);
 
-  if (tiktokCharactersLeft < 0) {
-    tiktokNumberEl.classList.add('stat__number--limit');
-  } else {
-    tiktokNumberEl.classList.remove('stat__number--limit');
-  }
-
-  if (instagramCharactersLeft < 0) {
-    instagramNumberEl.classList.add('stat__number--limit');
-  } else {
-    instagramNumberEl.classList.remove('stat__number--limit');
-  }
-
-  if (linkedinCharactersLeft < 0) {
-    linkedinNumberEl.classList.add('stat__number--limit');
-  } else {
-    linkedinNumberEl.classList.remove('stat__number--limit');
-  }
-
-  if (youtubeCharactersLeft < 0) {
-    youtubeNumberEl.classList.add('stat__number--limit');
-  } else {
-    youtubeNumberEl.classList.remove('stat__number--limit');
-  }
-
-  if (pinterestCharactersLeft < 0) {
-    pinterestNumberEl.classList.add('stat__number--limit');
-  } else {
-    pinterestNumberEl.classList.remove('stat__number--limit');
-  }
-
-  if (redditCharactersLeft < 0) {
-    redditNumberEl.classList.add('stat__number--limit');
-  } else {
-    redditNumberEl.classList.remove('stat__number--limit');
-  }
-
-  // set new numbers
+  // set new numbers for other indicators
   wordsNumberEl.textContent = numberOfWords;
   charactersNumberEl.textContent = numberOfCharacters;
   sentencesNumberEl.textContent = numberOfSentences;
-  paragraphsNumberEl.textContent = numberOfParagraphs;
-
-
-
-
+  paragraphsNumberEl.textContent = numberOfParagraphs + 1;
+  
+  // set new numbers for social media indicators 
   twitterNumberEl.textContent = twitterCharactersLeft;
   facebookNumberEl.textContent = facebookCharactersLeft;
   tiktokNumberEl.textContent = tiktokCharactersLeft;
